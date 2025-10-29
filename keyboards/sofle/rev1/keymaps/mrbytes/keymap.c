63517f2a89983b2886fd1e10bd96626ed2c9fa4a
mariorocha2308 <virgoroch852@gmail.com>
2025-10-29 08:59:40 -0600

    add config rgb matrix

--- keyboards/sofle/rev1/keymaps/mrbytes/keymap.c
+++ keyboards/sofle/rev1/keymaps/mrbytes/keymap.c
@@ -11,56 +11,6 @@ enum sofle_layers {
     _ADJUST,
 };
 
-// Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
-// const rgblight_segment_t PROGMEM capslock[] = RGBLIGHT_LAYER_SEGMENTS(
-//     {6, 4, HSV_GREEN},       // Light 4 LEDs, starting with LED 6
-//     {12, 4, HSV_GREEN}       // Light 4 LEDs, starting with LED 12
-// );
-// Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
-// const rgblight_segment_t PROGMEM qwerty[] = RGBLIGHT_LAYER_SEGMENTS(
-//     {0, 2, HSV_CYAN}
-// );
-// // Light LEDs 11 & 12 in purple when keyboard layer 2 is active
-// const rgblight_segment_t PROGMEM lower[] = RGBLIGHT_LAYER_SEGMENTS(
-//     {0, 2, HSV_RED}
-// );
-// // Light LEDs 13 & 14 in green when keyboard layer 3 is active
-// const rgblight_segment_t PROGMEM raise[] = RGBLIGHT_LAYER_SEGMENTS(
-//     {0, 2, HSV_PURPLE}
-// );
-// // Light LEDs 13 & 14 in green when keyboard layer 3 is active
-// // const rgblight_segment_t PROGMEM adjust[] = RGBLIGHT_LAYER_SEGMENTS(
-// //     {13, 2, HSV_YELLOW}
-// // );
-//
-// // Now define the array of layers. Later layers take precedence
-// const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-//     qwerty,    // Overrides caps lock layer
-//     lower,    // Overrides other layers
-//     raise // Overrides other layers
-//     // adjust
-// );
-//
-// void keyboard_post_init_user(void) {
-//     // Enable the LED layers
-//     rgblight_layers = my_rgb_layers;
-// }
-//
-// // bool led_update_user(led_t led_state) {
-// //     rgblight_set_layer_state(0, led_state.caps_lock);
-//     //     return true;
-//     // }
-//
-//     layer_state_t default_layer_state_set_user(layer_state_t state) {
-//         rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
-//         return state;
-//     }
-//
-//     layer_state_t layer_state_set_user(layer_state_t state) {
-//         rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
-//         rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
-//         return state;
-//     }
 
     const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
